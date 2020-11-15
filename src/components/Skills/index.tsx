f556077e18fed1484f28db6cb44484fab720ce96
Vy Nguyen <vynguyen9699@gmail.com>
2020-11-15 22:47:14 +0200

    create mobile version

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -33,6 +33,68 @@ const Skills = () => {
             <span className="skills__set">Others:</span> Git, GitHub, Jira.
           </li>
         </ul>
+        <div className="skills__icon-container">
+          <img
+            src="https://img.icons8.com/color/48/000000/javascript.png"
+            alt="javascript"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/typescript.png"
+            alt="typescript"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/react-native.png"
+            alt="react"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/redux.png"
+            alt="redux"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/html-5.png"
+            alt="html"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/css3.png"
+            alt="css"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/sass.png"
+            alt="sass"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/nodejs.png"
+            alt="node"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/mongodb.png"
+            alt="mongoDB"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/postgreesql.png"
+            alt="postgresQl"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/firebase.png"
+            alt="firebase"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/graphql.png"
+            alt="graphQl"
+          />
+          <img
+            src="https://img.icons8.com/fluent/48/000000/github.png"
+            alt="github"
+          />
+          <img
+            src="https://img.icons8.com/color/48/000000/jira.png"
+            alt="jira"
+          />
+          <img
+            src="https://img.icons8.com/officel/48/000000/accessibility2.png"
+            alt="accessibility"
+          />
+        </div>
       </div>
     </section>
   )
